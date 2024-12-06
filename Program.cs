e196f450ac802527971c20d24e6e06e1e2d9a83c
Alejandro G.M <agarciamears@microsoft.com>
2024-12-06 17:26:17 +0000

    Cambios NUevos en archivos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+
 
 namespace dotnetcore
 {
@@ -6,7 +11,35 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            string secretName = "MySecret";
+            string KeyVaultName = "my-vault-example-test";
+            var kvUri = "https://my-vault-example-test.vault.azure.net";
+            SecretClientOptions options = new SecretClientOptions(){
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(),options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+            Console.WriteLine("GetSecret: " + secret.Value);
+            Console.WriteLine("Enter Secret > ");
+            String secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+            Console.WriteLine("SetSecret:");
+            Console.WriteLine("SetSecret:");
+            Console.WriteLine(" Key: " + secretName);
+            Console.WriteLine(" Value:" + secretValue);
+
+            Console.WriteLine("GetSecret: " + secret.Value);
+
+            client.StartDeleteSecret(secretName);
+            Console.WriteLine("StartDeleteSecret" + KeyVaultName);
+
+            Console.WriteLine("GetSecret: " + secret.Value);
+
+        } 
     }
 }
